0e3b58ab84e17434f051138c0902c770aae55ff9
gael amand <gael.amand@gmail.com>
2023-08-22 11:35:40 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="bg-red-500 text-white p-4">Hello Tailwind CSS!</div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
